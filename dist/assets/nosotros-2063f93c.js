3f4283cbcaa7c68f3832268382df0b07f3d20549
andres-saa <andrew19f@gmail.com>
2025-06-18 14:59:03 -0500

    inicio

---
+++ dist/assets/nosotros-2063f93c.js
@@ -0,0 +1,31 @@
+import{o as i,c as s,a as e,F as n,r as t,t as r,b as c}from"./index-46e51a7b.js";const l={class:"continer shadow-1 mx-auto md:py-6 d-none",style:{"max-width":"1200px","margin-top":"5rem","background-color":"white"}},d=e("h1",{class:"text-center py-6"},[e("b",null," NOSOTROS ")],-1),p={class:"md:px-6"},m=["id"],u={key:0,class:"grid",style:{"align-items":"center"}},h={class:"col-12 md:col-6"},v=["innerHTML"],g={class:"col-12 md:col-6"},_=["src"],y={key:1,class:"grid my-2 hiden",style:{"align-items":"center"}},w={class:"col-12 md:col-6"},f=["src"],b={class:"col-12 md:col-6"},x=["innerHTML"],j={key:2,class:"grid replace"},T={class:"col-12 md:col-6"},L=["innerHTML"],M={class:"col-12 md:col-6"},k=["src"],R={__name:"nosotros",setup(O){const a=[{name:"QUIENES SOMOS?",img:"https://academia.crandi.com/wp-content/uploads/2020/07/Mision2-1024x661.jpg",text:"ING MASTER es símbolo de perseverancia una empresa caleña que lleva en el mercado más de 15 años de experiencia en refrigeración; fabricando de cuartos fríos y venta de panel con un alto equipo especializado en estos  servicios.",hor:"img-izq",videoLink:"https://www.youtube.com/embed/TqRjQswjWCs?si=hvOXtWdd28_hfxfE",id:"historia"},{name:"Mision",img:"https://academia.crandi.com/wp-content/uploads/2020/07/Mision2-1024x661.jpg",text:"Brindamos la mejor tecnología en el campo de la refrigeración, permitiéndonos así, ser líderes a nivel nacional y ofrecer la mejor calidad y el mejor precio.",hor:"img-der",videoLink:"https://www.youtube.com/embed/TqRjQswjWCs?si=hvOXtWdd28_hfxfE",id:"mision"},{name:"Vision",img:"https://academia.crandi.com/wp-content/uploads/2020/07/Mision2-1024x661.jpg",text:"Posicionarnos como líderes a nivel nacional en el mercado, trabajando siempre de la mano con los mejores proveedores e impulsar la sostenibilidad con nuestro producto y servicio.",hor:"img-izq",videoLink:"https://www.youtube.com/embed/TqRjQswjWCs?si=hvOXtWdd28_hfxfE",id:"vision"},{name:"Nuestros valores",img:"https://academia.crandi.com/wp-content/uploads/2020/07/Mision2-1024x661.jpg",text:`<div class="valor">
+                <strong>Respeto:</strong>
+                <p>En ING MASTER, valoramos el respeto al escuchar y apreciar la diversidad de opiniones, fomentando relaciones sólidas con clientes, proveedores y colaboradores.</p>
+            </div>
+
+            <br>
+            <div class="valor">
+                <strong>Responsabilidad:</strong>
+                <p>Nos comprometemos a cumplir con nuestras obligaciones y a entregar proyectos con precisión y en el tiempo acordado, garantizando calidad y profesionalismo.</p>
+            </div>
+
+            <br> 
+
+            <div class="valor">
+                <strong>Calidad:</strong>   
+                <p>Ofrecemos productos y servicios de alta calidad en refrigeración industrial, con controles rigurosos y un enfoque en la excelencia e innovación.</p>
+            </div>
+
+            <br>
+
+            <div class="valor">
+                <strong>Honestidad:</strong>
+                <p>Operamos con transparencia y claridad, construyendo confianza mediante prácticas justas e integridad en todas nuestras interacciones.</p>
+            </div>
+
+            <br>
+            <div class="valor">
+                <strong>Pasión:</strong>
+                <p>Nuestra pasión por la refrigeración industrial nos impulsa a ofrecer un servicio excepcional y a buscar constantemente nuevas formas de mejorar.</p>
+            </div>
+`,hor:"img-der",videoLink:"https://www.youtube.com/embed/TqRjQswjWCs?si=hvOXtWdd28_hfxfE",id:"valores"}];return(q,E)=>(i(),s("div",l,[d,e("div",p,[(i(),s(n,null,t(a,o=>e("div",{class:"col-12 pt-2",id:o.id},[o.hor=="img-der"?(i(),s("div",u,[e("div",h,[e("h3",null,[e("b",null,r(o.name),1)]),e("h6",{style:{"text-align":"justify"},innerHTML:o.text},null,8,v)]),e("div",g,[e("iframe",{width:"100%",height:"315",src:o.videoLink,title:"YouTube video player",frameborder:"0",allow:"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share",referrerpolicy:"strict-origin-when-cross-origin",allowfullscreen:""},null,8,_)])])):(i(),s("div",y,[e("div",w,[e("iframe",{width:"100%",height:"315",src:o.videoLink,title:"YouTube video player",frameborder:"0",allow:"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share",referrerpolicy:"strict-origin-when-cross-origin",allowfullscreen:""},null,8,f)]),e("div",b,[e("h3",null,[e("b",null,r(o.name),1)]),e("h6",{style:{"text-align":"justify"},innerHTML:o.text},null,8,x)])])),o.hor=="img-izq"?(i(),s("div",j,[e("div",T,[e("h3",null,[e("b",null,r(o.name),1)]),e("h6",{style:{"text-align":"justify"},innerHTML:o.text},null,8,L)]),e("div",M,[e("iframe",{width:"100%",height:"315",src:o.videoLink,title:"YouTube video player",frameborder:"0",allow:"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share",referrerpolicy:"strict-origin-when-cross-origin",allowfullscreen:""},null,8,k)])])):c("",!0)],8,m)),64))])]))}};export{R as default};
